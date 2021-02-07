e1886eac62fe0105a8f59a6f315be349114b7d71
Jefferson Alves <jeffersonalmeida16@outlook.com>
2021-02-07 07:45:03 -0300

    eureka_server

---
+++ openfly-eureka_server/src/test/java/com/alvesjefs/eureka/OpenflyEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvesjefs.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenflyEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
